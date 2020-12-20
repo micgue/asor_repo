4399fa89ee31d573929a73aa09f734b2c53ce80c
root <root@localhost.localdomain>
2020-12-20 21:40:26 +0100

    Fin, fallo ejer17

---
+++ practica2.2/ejercicio17.c
@@ -0,0 +1,15 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+
+
+return 0;
+}
