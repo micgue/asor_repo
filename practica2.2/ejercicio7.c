14e7ac05e82ab4ffe65fe162a700b25a4c311782
root <root@localhost.localdomain>
2020-12-20 18:38:18 +0100

    commit ejercicio 5,6,7

--- practica2.2/ejercicio7.c
+++ practica2.2/ejercicio7.c
@@ -6,11 +6,15 @@
 #include <string.h>
 int main(){
 int errno;
-int file = open("prueba5",O_CREAT, 0645);
+/*parte de ejercicio 7*/
+mode_t nuevo = umask(027);
+int file = open("prueba7",O_CREAT, 0645);
 if(file == -1){
 printf("Error: %s\n", strerror(errno));
 }
 close(file);
+/*parte ejercicio7*/
+printf("Valor Umask: %i\n", umask(nuevo));
 return 0;
 }
 
