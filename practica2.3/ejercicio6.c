604dda44fde357180a554577b9ea74209015e784
root <root@localhost.localdomain>
2021-01-16 02:29:22 +0100

    Ejercicio 5 y 6 + respuesta

--- practica2.3/ejercicio6.c
+++ practica2.3/ejercicio6.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<errno.h>
+
+#include<string.h>
 #include<sys/types.h>
 #include <unistd.h>
 #include<sys/time.h>
 #include<sys/resource.h>
 #include<stdlib.h>
 
-int main(){
-pid_t pid = getpid();
+void aux(){
+pid_t pid= getpid();
 printf("Identificador de proceso: %i\n", pid);
 pid_t ppid = getppid();
 printf("Identificador de proceso padre: %i\n", ppid);
@@ -19,14 +21,37 @@ struct rlimit rlim;
 int err = getrlimit(RLIMIT_FSIZE, &rlim);
 if(err == -1){
 printf("Error\n");
-return -1;
 }
 printf("Numero maximo de ficheros que puede abrir el proceso: %i\n", rlim.rlim_max);
 size_t size = 1000;
 char *buffer = malloc(sizeof(char) * size);
 printf("Path: %s\n", getcwd(buffer, size));
 free(buffer);
-return 0;
 }
 
+
+int main(){
+pid_t pid = fork(); // creación del nuevo proceso 
+switch (pid) {
+case -1:
+perror("Error fork\n");
+exit(1);
+case 0: /* ejecutando proceso hijo, se crea una nueva sesion, nueva ruta a /tmp*/
+;pid_t child_sid = setsid(); 
+if(chdir("/tmp") == -1){
+printf("Error cambio directorio\n");
+return -1;
+}
+printf("Proceso hijo....\n");
+aux();
+//sleep(10);
+ break;
+
+default:
+printf("Proceso padre...\n");
+aux();
+  break;
+}
+return 0;
+}
 
